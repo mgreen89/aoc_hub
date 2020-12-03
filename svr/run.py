0fc12e6e23243dd34a4c6bbaf2c2de0a59faa618
Jackson Riley <jackson.riley@btinternet.com>
2020-12-03 23:01:42 +0000

    Clean up backend

--- svr/run.py
+++ svr/run.py
@@ -24,27 +24,24 @@ log = logging.getLogger(__name__)
 
 class UserDatabaseFeature(ConfiguredFeature):
     """
-    Feature that runs a shell command with zero security
+    Simple feature to persist a list of users.
     """
 
     # Feature name, started in config.yml (or by name by the client if it were
     # a dynamic feature instead)
     name = "user_database"
 
-    # Message schema: accept one request, named 'store_new_user', with one
-    # argument, of name 'cmd', and send an RPC reply.
+    # Message schema: accept:
+    #   store_new_user: args - name, url, languages
+    #   get_all_users: no args
     requests = {"store_new_user": ["name", "url", "languages"],
                 "get_all_users": []}
 
-    # do_thing is called whenever a request of name 'thing' is received. So
-    # this is the implementation of the 'insecure_shell_cmd' request. This
-    # should always return exactly one of:
-    #  - self._rpc_success(value)
-    #  - self._rpc_failure(err_string).
     async def do_store_new_user(self, name, url, languages):
-        log.info("Received store_new_user[{}, {}, {}]".format(name, url, languages))
+        log.info("Received store_new_user request[{}, {}, {}]".format(name, url, languages))
 
         if not os.path.exists('users.json'):
+            # Create an empty file
             with open('users.json', 'w'): pass
 
         with open("users.json", "r") as f:
@@ -52,9 +49,9 @@ class UserDatabaseFeature(ConfiguredFeature):
                 users = json.load(f)
                 log.info("Loaded: {}".format(users))
             except json.decoder.JSONDecodeError:
-                users = {}
+                users = []
 
-        users[name] = {"name": name, "url": url, "languages": languages}
+        users.append({"name": name, "url": url, "languages": languages})
         with open("users.json", "w") as f:
             json.dump(users, f)
 
@@ -65,21 +62,17 @@ class UserDatabaseFeature(ConfiguredFeature):
     async def do_get_all_users(self):
         log.info("Received get_all_users")
         if not os.path.exists('users.json'):
-            users = {}
+            users = []
         else:
             with open("users.json", "r") as f:
                 try:
                     users = json.load(f)
                     log.info("Loaded: ", users)
                 except json.decoder.JSONDecodeError:
-                    users = {}
-        # Want to return 
-        #     { users: "[{"name": "Alice", "url": "some-url", "languages": "C, Python"},
-        #                {"name": "Bob", "url": "some-other-url", "languages": "Rust"}]"
-        #     }
-        json_list = [v for (_, v) in users.items()]
-        log.info("Sending {}".format(json.dumps(json_list)))
-        return self._rpc_success(json_list)
+                    users = []
+
+        log.info("Sending get_all_users response: {}".format(users))
+        return self._rpc_success(users)
 
 
 # Start up
